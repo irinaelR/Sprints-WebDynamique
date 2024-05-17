786a16ef750906d3f8a8712b91ceb31ee171b164
El <irina.elina.r@gmail.com>
2024-05-17 11:51:04 +0300

    Création des nouvelles classes
    - Get est une annotation de méthode contenant un string pour l'url qui va appeler cette méthode
    - Mapping est une classe utilitaire pour afficher la classe et la méthode correspondant à une annotation Get

---
+++ src/framework/utilities/Mapping.java
@@ -0,0 +1,26 @@
+package framework.utilities;
+
+public class Mapping {
+    String className;
+    String methodName;
+    
+    public String getClassName() {
+        return className;
+    }
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    public String getMethodName() {
+        return methodName;
+    }
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public Mapping(String className, String methodName) {
+        this.className = className;
+        this.methodName = methodName;
+    }
+    public Mapping() {
+    }
+}
