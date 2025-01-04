73b44d442b4f49169f599f2e67b020a37ea50e6c
El <irina.elina.r@gmail.com>
2025-01-04 14:03:33 +0300

    Tweak

--- src/framework/annotations/Max.java
+++ src/framework/annotations/Max.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 public @interface Max {
     double value();
-    boolean isStrict() default false;
+    boolean isStrict() default true;
 }
--- src/framework/annotations/Min.java
+++ src/framework/annotations/Min.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 public @interface Min {
     double value();
-    boolean isStrict() default false;
+    boolean isStrict() default true;
 }
